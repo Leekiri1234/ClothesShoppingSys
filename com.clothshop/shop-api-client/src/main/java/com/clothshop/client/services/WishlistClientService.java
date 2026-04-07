6c213ff0bb73e72285535e288931834a947ad1a6
Bonny <bonnynguyen456@gmail.com>
2026-04-07 13:25:15 +0700

    fix wishlismapper

--- com.clothshop/shop-api-client/src/main/java/com/clothshop/client/services/WishlistClientService.java
+++ com.clothshop/shop-api-client/src/main/java/com/clothshop/client/services/WishlistClientService.java
@@ -91,13 +91,6 @@ public class WishlistClientService {
             return true;
         }
 
-        // Nếu tồn tại → toggle
-        boolean newStatus = !Boolean.TRUE.equals(item.getIsActive());
-        item.setIsActive(newStatus);
-        wishlistItemRepository.save(item);
-
-        log.info("Toggle product {} in wishlist of user {} → {}", productId, username, newStatus);
-        return newStatus;
     }
 
     @Transactional // CHỐT: Bỏ readOnly = true vì hàm này gọi getOrCreateWishlist (có thể gây INSERT)
