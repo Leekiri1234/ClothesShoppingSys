2c1801ad569266158edbfffed37a5b78e5d217c3
honggduc <148782719+Leekiri1234@users.noreply.github.com>
2026-04-17 10:29:32 +0700

    Delete tmp_replace.py

--- tmp_replace.py
+++
@@ -1,7 +0,0 @@
-﻿from pathlib import Path
-path = Path('com.clothshop/shop-api-admin/src/main/resources/templates/admin/dashboard.html')
-for i,line in enumerate(path.read_text(encoding='utf-8').splitlines(), start=1):
-    if 'revenue-list' in line:
-        print('revenue-list at', i)
-    if 'top-products-list' in line:
-        print('top-products-list at', i)
